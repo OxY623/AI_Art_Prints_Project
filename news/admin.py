41e533735d877e5bf78fc1c6eca2c71326e29839
OxY623 <fuzzydoorproductions0@gmail.com>
2023-06-12 17:22:55 +0300

    Summary

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Article
+from prints.admin import admin_site
 
 # Register your models here.
-admin.site.register(Article)
+# admin.site.register(Article)
+admin_site.register(Article)
